6fabc29f6ba83f82e237dc7673dd934cc8b4fd91
memo40 <memocann4040@gmail.com>
2020-12-21 22:11:15 +0100

    finished js

--- script.js
+++ script.js
@@ -1,43 +1,42 @@
-var block= document.getElementById("block");
+var block = document.getElementById("block");
 var hole = document.getElementById("hole");
-var char = document.getElementById("character");
-let jumping = 0;
+var character = document.getElementById("character");
+var jumping = 0;
+var counter = 0;
 
-hole.addEventListener("animationiteration",()=>{
+hole.addEventListener('animationiteration', () => {
     var random = -((Math.random()*300)+150);
-    
-    hole.style.top= random + "px";
-})
-
-
-setInterval(() => {
-    var characterpos =
-     parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    hole.style.top = random + "px";
+    counter++;
+});
+setInterval(function(){
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     if(jumping==0){
-     character.style.top=(characterpos+3)+"px";}
-    
-       var blockleft= parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-        var holetop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
-        var charactertop= parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-        var cTop =-(500-charactertop);
-     if((characterpos>450)||(blockleft<20)&&(blockleft>-50)&&((cTop<holetop)||(ctop>holetop+130))){
-        alert("game over");
-        character.style.top=100+"px";
+        character.style.top = (characterTop+3)+"px";
     }
- 
-}, 10);
-
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+    var cTop = -(500-characterTop);
+    if((characterTop>480)||((blockLeft<20)&&(blockLeft>-50)&&((cTop<holeTop)||(cTop>holeTop+130)))){
+        alert("Game over. Score: "+(counter-1));
+        character.style.top = 100 + "px";
+        counter=0;
+    }
+},10);
 
 function jump(){
-
-let jumpcounter=0;
-var jumpinterval = setInterval(() => {
-    var characterpos = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    character.style.top=(characterpos-5)+"px";
-
-    if(jumpcounter>20){
-        clearInterval(jumpinterval);
-    }
-    jumpcounter++;
-}, 10);
+    jumping = 1;
+    let jumpCount = 0;
+    var jumpInterval = setInterval(function(){
+        var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+        if((characterTop>6)&&(jumpCount<15)){
+            character.style.top = (characterTop-5)+"px";
+        }
+        if(jumpCount>20){
+            clearInterval(jumpInterval);
+            jumping=0;
+            jumpCount=0;
+        }
+        jumpCount++;
+    },10);
 }
