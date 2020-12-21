d5cf41a0fbe3ab128da2d2e0ec2e836b3fe8febc
memo40 <memocann4040@gmail.com>
2020-12-21 21:32:20 +0100

    setup

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 var block= document.getElementById("block");
 var hole = document.getElementById("hole");
 var char = document.getElementById("character");
-var jumping = 0;
+let jumping = 0;
 
 hole.addEventListener("animationiteration",()=>{
     var random = -((Math.random()*300)+150);
-    if(jumping==0){
-    hole.style.top= random + "px";}
+    
+    hole.style.top= random + "px";
 })
 
 
 setInterval(() => {
-    var characterpos = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    character.style.top=(characterpos+3)+"px";
-    if(characterpos>450){
+    var characterpos =
+     parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    if(jumping==0){
+     character.style.top=(characterpos+3)+"px";}
+    
+       var blockleft= parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+        var holetop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+        var charactertop= parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+        var cTop =-(500-charactertop);
+     if((characterpos>450)||(blockleft<20)&&(blockleft>-50)&&((cTop<holetop)||(ctop>holetop+130))){
         alert("game over");
         character.style.top=100+"px";
     }
+ 
 }, 10);
 
 
 function jump(){
-jumping=1;
+
 let jumpcounter=0;
 var jumpinterval = setInterval(() => {
     var characterpos = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
