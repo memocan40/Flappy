06d3d13c51d522edfcb63147c38a16b2a56ad8f1
memo40 <memocann4040@gmail.com>
2020-12-21 20:35:06 +0100

    addet functions

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 var block= document.getElementById("block");
 var hole = document.getElementById("hole");
-
+var char = document.getElementById("character");
+var jumping = 0;
 
 hole.addEventListener("animationiteration",()=>{
     var random = -((Math.random()*300)+150);
-    hole.style.top= random + "px";
+    if(jumping==0){
+    hole.style.top= random + "px";}
 })
+
+
+setInterval(() => {
+    var characterpos = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    character.style.top=(characterpos+3)+"px";
+}, 10);
+
+
+function jump(){
+jumping=1;
+let jumpcounter=0;
+var jumpinterval = setInterval(() => {
+    var characterpos = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    character.style.top=(characterpos-5)+"px";
+    jumpcounter++;
+}, 10);
+}
